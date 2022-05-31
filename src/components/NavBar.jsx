3ac7bc2cbd92f822c1d6e3f6211da875ec0d5463
Mohammed <d2018c@gmail.com>
2022-05-31 17:48:31 +0100

    nav bar with react routing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
+import Home from "./Home";
+// import NavBar from "./NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import { Link } from 'react-router-dom'
 
-export default function NavBar() {
+function NavBar() {
+  const navStyle = {
+    color:"white"
+  }
   return (
-    <div>NavBar</div>
+    <div>
+    <Router>
+   
+         
+    <div>
+       <nav className={navStyle}>
+         <h3>Logo</h3>
+         <ul>
+           <Link to="/"></Link>
+           <li>Home</li>
+           <Link to="/about">
+           <li>About</li>
+           </Link>
+           <Link to="/contact">
+           <li>Contact</li>
+           </Link>
+           </ul>
+   
+        
+      </nav>
+    </div>
+
+     <Routes>
+     <Route exact path="/" element={Home}/>
+     <Route exact path="/about" element={About}/>
+
+     <Route exact path="/contact" element={Contact}/>
+     
+   
+    
+      </Routes>
+      
+
+  
+
+    </Router>
+    </div>
   )
 }
+
+export default NavBar;
