bcfc99b507c764db3afb723a9b0b89eecbd53ada
Mohammed <d2018c@gmail.com>
2022-06-21 16:33:17 +0100

    index css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header> 
+      <div className="container header__container">
+      <h5>Hello I'm </h5>
+      <h1>Mohammed Chowdhury</h1>
+      <h5 className="text-light">Front-End Developer</h5>   
+      </div>   
+      </header>
   )
 }
