b128db197db4dc61260eacf95c4f762ab28406cd
Mohammed <d2018c@gmail.com>
2022-10-05 18:11:16 +0100

    portfolio cta

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from "react";
+import CV from "../../assets/Mohammed-Front-End-Developer-CV.docx";
 export default function CTA() {
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Contact me</a>
-
-    
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Contact me
+      </a>
     </div>
-  )
+  );
 }
